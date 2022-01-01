ea31be5358507e095599a815650836a4dd6203ff
g-prog <alaobukky2@gmail.com>
2022-01-01 07:42:46 -0800

    almost done building out the admin page

--- tedbree/src/Admin.jsx
+++ tedbree/src/Admin.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import SearchIcon from '@mui/icons-material/Search';
 
 
@@ -18,7 +19,7 @@ function Admin() {
                 </div>
                 <div className='flex'>
                     <NotificationsIcon style={{color: "white",  marginTop: 10 }}/>
-                    <img  className='w-12 rounded-full ml-4' src="../assets/job2.jpg"/>
+                    <img  className='w-12 rounded-full ml-4' src="../assets/job2.jpg" alt='logo'/>
                 </div>
             </nav>
             <div className="w-6/12 ml-64">
@@ -40,6 +41,19 @@ function Admin() {
                     <p className='text-white mt-2'>New Job</p>
 
                 </button>
+            </div>
+
+            <div className='flex bg-sky-900 w-11/12 mt-10 ml-4 rounded-md py-2 px-2 justify-around'>
+                <div className='text-white'>Job Title</div>
+                <div className='text-white'>Candidates</div>
+                <div className='text-white'>Date Modified</div>
+                <div className='flex'>
+                    <p className='text-white'>Filter</p>
+                    <FilterAltIcon  style={{color: "white", marginLeft: 5 }}/>
+
+
+                </div>
+
             </div>
 
         </div>
