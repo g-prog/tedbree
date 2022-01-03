1f2d45c3697983e1dd5898f3e7b73f14386398c6
g-prog <alaobukky2@gmail.com>
2022-01-03 08:21:53 -0800

    made the footer mobile responsive

--- tedbree/src/Footer.js
+++ tedbree/src/Footer.js
@@ -9,8 +9,8 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <div className='bg-sky-900 w-full py-20  md:flex md:justify-around'>
-            <div className='flex flex-col mt-4 md:mt-0'>
+        <div className='bg-sky-900 w-full py-20 md:flex md:justify-around'>
+            <div className='flex flex-col mt-4  ml-28 md:ml-0 md:mt-0'>
                 <h1 className="text-white font-bold font-serif italic origin-bottom -rotate-2 text-2xl text-sky-100">FIND JOBS</h1>
                 <div className='flex'>
                     <CopyrightIcon style={{ fontSize: 20, marginTop: 30, color: "white"  }}/>
@@ -20,7 +20,7 @@ function Footer() {
                 
 
             </div>
-            <div className='flex flex-col mt-4 md:mt-0'>
+            <div className='flex flex-col md:mt-4 mt-10 ml-28 md:ml-0 md:mt-0'>
                 <h1 className='text-white text-lg font-bold'>Quick links</h1>
                 <Link to='/'>
                     <p className='text-white text-sm font-medium'>Home</p>
@@ -39,7 +39,7 @@ function Footer() {
 
             </div>
 
-            <div className='flex flex-col mt-4 md:mt-0'>
+            <div className='flex flex-col mt-10 md:mt-4  ml-28 md:ml-0 md:mt-0'>
                 <h1 className='text-white text-lg font-bold'>Quick links</h1>
                 <Link to='/'>
                     <p className='text-white text-sm font-medium'>Home</p>
@@ -57,18 +57,18 @@ function Footer() {
 
             </div>
 
-            <div className='flex flex-col mt-4 md:mt-0'>
-                <h1 className='text-white text-lg font-bold ml-4'>Social Media</h1>
-                <div className='flex justify-between'>  
-                    <div className='bg-sky-600 rounded-full px-2 py-0.5 mt-4 ml-4'>
+            <div className='flex flex-col md:mt-4 mt-10  ml-28 md:ml-0 md:mt-0'>
+                <h1 className='text-white text-lg font-bold ml-2 md:ml-4'>Social Media</h1>
+                <div className='flex md:justify-between'>  
+                    <div className='bg-sky-600 rounded-full px-2 py-0.5 mt-4 ml-2 md:ml-4'>
                         <InstagramIcon  style={{ fontSize: 20, marginTop: 5, color: "white", marginLeft: 2}}/>
                         
                     </div>
-                    <div className='bg-sky-600 rounded-full px-2 py-1.5 mt-4 ml-4'>
+                    <div className='bg-sky-600 rounded-full px-2 py-1.5 mt-4  ml-2 md:ml-4'>
                         <FacebookIcon style={{ fontSize: 20, marginTop: 1, color: "white" }}/>
                         
                     </div>
-                    <div className='bg-sky-600 rounded-full px-2 py-1 mt-4 ml-4'>
+                    <div className='bg-sky-600 rounded-full px-2 py-1 mt-4 ml-2 md:ml-4'>
                         <TwitterIcon style={{ fontSize: 20, marginTop: 1, color: "white", marginLeft: 2  }}/>
                         
                     </div>
