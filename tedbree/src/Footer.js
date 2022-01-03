c101042982110ec1a8e377eb7aa7ef1682d3d0b8
g-prog <alaobukky2@gmail.com>
2022-01-03 12:13:29 -0800

    perfected some things

--- tedbree/src/Footer.js
+++ tedbree/src/Footer.js
@@ -10,7 +10,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <div className='bg-sky-900 w-full py-20 md:flex md:justify-around'>
-            <div className='flex flex-col mt-4  ml-28 md:ml-0 md:mt-0'>
+            <div className='flex flex-col mt-4  ml-32 md:ml-0 md:mt-0'>
                 <h1 className="text-white font-bold font-serif italic origin-bottom -rotate-2 text-2xl text-sky-100">FIND JOBS</h1>
                 <div className='flex'>
                     <CopyrightIcon style={{ fontSize: 20, marginTop: 30, color: "white"  }}/>
@@ -57,7 +57,7 @@ function Footer() {
 
             </div>
 
-            <div className='flex flex-col md:mt-4 mt-10  ml-28 md:ml-0 md:mt-0'>
+            <div className='flex flex-col md:mt-4 mt-10  ml-32 md:ml-0 md:mt-0'>
                 <h1 className='text-white text-lg font-bold ml-2 md:ml-4'>Social Media</h1>
                 <div className='flex md:justify-between'>  
                     <div className='bg-sky-600 rounded-full px-2 py-0.5 mt-4 ml-2 md:ml-4'>
